1e0996d794e6b9436a98e299252aec57361facd7
Jannik Zschiesche <jz@becklyn.com>
2019-06-28 12:37:21 +0200

    Add return types

--- js/lib/icons.tsx
+++ js/lib/icons.tsx
@@ -1,10 +1,10 @@
-import {h} from "preact";
+import {h, JSX} from "preact";
 
 
 /**
  * For the dropdown indicator
  */
-export function ChevronIcon ()
+export function ChevronIcon (): JSX.Element
 {
     return <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M6 9l6 6 6-6"/></svg>;
 }
@@ -12,7 +12,7 @@ export function ChevronIcon ()
 /**
  * For the check mark inside selected choices
  */
-export function CheckIcon ()
+export function CheckIcon (): JSX.Element
 {
     return <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M9.3 18.7L2.7 12l1.8-1.9 4.8 4.8L19.5 4.8l1.8 1.9z" /></svg>;
 }
@@ -20,7 +20,7 @@ export function CheckIcon ()
 /**
  * As prefix for the search input
  */
-export function SearchIcon ()
+export function SearchIcon (): JSX.Element
 {
     return <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="11" cy="11" r="8"/><path d="M21 21l-4.35-4.35"/></svg>;
 }
@@ -28,7 +28,7 @@ export function SearchIcon ()
 /**
  * The loading spinner
  */
-export function LoadingIcon ()
+export function LoadingIcon (): JSX.Element
 {
     return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 15 15" width="15" height="15"><path d="M7.5 1.5a6 6 0 0 1 0 12V15A7.5 7.5 0 1 0 0 7.5h1.5a6.018 6.018 0 0 1 6-6z" /></svg>;
 }
@@ -37,7 +37,7 @@ export function LoadingIcon ()
 /**
  * Closing / deleting icon
  */
-export function DeleteIcon ()
+export function DeleteIcon (): JSX.Element
 {
     return <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18" /><line x1="6" y1="6" x2="18" y2="18" /></svg>;
 }
