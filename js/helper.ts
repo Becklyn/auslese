fcad4f42cc64a73401345851d9fbd229981c52d5
Jannik Zschiesche <jz@becklyn.com>
2020-06-26 17:53:52 +0200

    Add `createChoiceGroup()` helper

---
+++ js/helper.ts
@@ -0,0 +1,13 @@
+import {AusleseTypes} from "./@types/auslese";
+
+
+/**
+ * Helper function to easily create choice groups from choices
+ */
+export function createChoiceGroup (choices: AusleseTypes.Choice|AusleseTypes.Choice[], headline: string|null = null) : AusleseTypes.Group
+{
+    return {
+        headline,
+        choices: Array.isArray(choices) ? choices : [choices],
+    };
+}
