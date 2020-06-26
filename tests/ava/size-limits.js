be8dd21b0116087a3bae1d350c1544e504f996d1
Jannik Zschiesche <jz@becklyn.com>
2020-06-26 18:27:28 +0200

    Increase size limit

--- tests/ava/size-limits.js
+++ tests/ava/size-limits.js
@@ -7,7 +7,7 @@ import Terser from "terser";
 let files = {
     'index.js': 75,
     'Auslese.js': 3700,
-    'automount.js': 1550,
+    'automount.js': 1600,
     'lib/helper.js': 1025,
     'lib/icons.js': 1350,
     'components/Choice.js': 500,
